af325e96bddcfc8387870e1f943f0f63ebd9bc27
cliffybar <crowellclifford@utexas.edu>
2021-03-20 13:25:58 -0500

    added script.js and pseudo-coded

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,18 @@
+// current day is displayed at top of calendar
+
+// var todayEl = document.querySelector("#currentDay");
+// todayEl.appendChild(now);
+
+// presented with time blocks for standard business hours (8-5)
+    // divide by hour
+
+//each time block is color-coded to indicate whether it is in the past, present, or future 
+    // add 3 colors for past, present, or future
+
+// "click" event into time block lets user enter an event input
+    // addEventListener("click", time-block)
+
+// text for event saved in local torage
+    // addEventListener, setItem, JSON, parse
+
+// refreshing page lets events persist
