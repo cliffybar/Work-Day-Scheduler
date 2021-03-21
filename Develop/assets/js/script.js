614f403c572b68717ae77125aa6ed03e3c00e25c
cliffybar <crowellclifford@utexas.edu>
2021-03-21 14:20:18 -0500

    added colors for past, present, or future. non-functional

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,16 +2,29 @@
 var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
 $('#currentDay').append(now);
 
-// presented with time blocks for standard business hours (9-5)
-    // divide by hour
-
 //each time block is color-coded to indicate whether it is in the past, present, or future 
     // add 3 colors for past, present, or future
+var hourOfDay = function () {
+    var presentHour = moment().hour()
 
+    $(".task-content").each(function() {
+        var pastHour = parseInt($(this).attr("id"))
+    
+        if (pastHour < presentHour) {
+            $(this).css('background-color', 'grey')
+        } else if (pastHour === presentHour) {
+            $(this).css('background-color', 'red')
+        } else if (pastHour > presentHour) {
+            $(this).css('background-color', 'green')
+        }
+    })
+} 
+
+hourOfDay()
 // "click" event into time block lets user enter an event input
     // addEventListener("click", time-block)
 
 // text for event saved in local torage
     // addEventListener, setItem, JSON, parse
 
-// refreshing page lets events persist
+// refreshing page lets events persist 
